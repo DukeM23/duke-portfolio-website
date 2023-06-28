6e851916d0da11241c9abef4b511b3ae45bce651
DukeM23 <dukemaquiling@gmail.com>
2023-06-27 22:53:44 -0400

    Removed unused imports

--- src/components/Navbars/Menu.tsx
+++ src/components/Navbars/Menu.tsx
@@ -1,6 +1,5 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import SideBar from "./SideBar";
 
